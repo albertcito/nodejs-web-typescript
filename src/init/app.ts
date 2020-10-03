c09e590089b59ff1a19ed12b270b4687cae2a76c
Albert Barrientos <albert.bar@gmail.com>
2020-10-03 11:12:13 -0400

    rmove redis

--- src/init/app.ts
+++ src/init/app.ts
@@ -1,16 +1,16 @@
 import 'reflect-metadata';
 import express, { Express } from 'express';
 import { ConnectionOptions, createConnection } from 'typeorm';
-import session from 'express-session';
+// import session from 'express-session';
 
 import initApolloServer from './apolloServer';
 import ormconfig from '../../ormconfig.json';
-import redisSession from '../config/redisSession';
+// import redisSession from '../config/redisSession';
 
 const getApp = async (): Promise<Express> => {
   const app = express();
   const db = await createConnection(ormconfig as ConnectionOptions);
-  app.use(session(redisSession));
+  //  app.use(session(redisSession));
   app.get('/test', (_, res) => {
     res.json({ hello: 'test' });
   });
