ac9115105b87d932d7812f5dcd2092ac7e6c8c23
Albert <>
2020-10-31 12:52:42 -0400

    using same function to report errors

--- src/init/bugsnag/notify.ts
+++ src/init/bugsnag/notify.ts
@@ -4,9 +4,10 @@ import Auth from '../../util/session/Auth';
 
 const notify = (error: Error, req: Request) => {
   const auth = Auth.data()?.auth;
+  const user = Auth.data()?.user;
   Bugsnag.notify(error, (event) => {
-    event.addMetadata('get', req.query);
-    event.addMetadata('post', req.body);
+    event.addMetadata('query', req.query);
+    event.addMetadata('body', req.body);
     event.addMetadata('request', {
       originalUrl: req.originalUrl,
       url: req.url,
@@ -17,7 +18,8 @@ const notify = (error: Error, req: Request) => {
       httpVersion: req.httpVersion,
       httpVersionMajor: req.httpVersionMajor,
       httpVersionMinor: req.httpVersionMinor,
-      userID: auth?.userID,
+      userID: user?.userID,
+      emailVerified: user?.emailVerified,
       oauthAccessTokenID: auth?.oauthAccessTokenID,
       ...req.headers,
     });
--- src/init/graphql/public/errorHandlePlugin.ts
+++ src/init/graphql/public/errorHandlePlugin.ts
@@ -1,39 +1,18 @@
 import { ApolloServerPlugin, GraphQLRequestContextWillSendResponse } from 'apollo-server-plugin-base';
 import { HttpQueryError } from 'apollo-server-core';
-import Bugsnag from '@bugsnag/js';
 
 import AuthenticationError from '../../../util/exceptions/AuthenticationError';
-import Auth from '../../../util/session/Auth';
 import isValidException from '../../../util/exceptions/isValidException';
 import { config } from '../../../config';
-
-const notify = (error: Error, context: GraphQLRequestContextWillSendResponse<any>) => {
-  const auth = Auth.data()?.auth;
-  Bugsnag.notify(error ?? error, (event) => {
-    event.addMetadata('graphql', {
-      source: context.source,
-      operationName: context.operationName,
-      variables: context.request.variables,
-      ...context.operation,
-    });
-    event.addMetadata('request', {
-      url: event.request.url,
-      httpMethod: event.request.httpMethod,
-      clientIp: event.request.clientIp,
-      referer: event.request.referer,
-      userID: auth?.userID,
-      oauthAccessTokenID: auth?.oauthAccessTokenID,
-      ...event.request.headers,
-    });
-  });
-};
+import { ApolloServerContext } from '../ApolloServerContext';
+import notify from '../../bugsnag/notify';
 
 // I tried to do it like the Apollo test, but its doesn't works
 // https://github.com/apollographql/apollo-server/blob/1af2792ed9e18f2bf218a29c2f4f128b6588f9ca/packages/apollo-server-integration-testsuite/src/ApolloServer.ts#L661
 const ErrorHandlePlugin: ApolloServerPlugin = {
   requestDidStart() {
     return {
-      willSendResponse(requestContext) {
+      willSendResponse(requestContext: GraphQLRequestContextWillSendResponse<ApolloServerContext>) {
         const errors = requestContext.errors ?? [];
         // eslint-disable-next-line no-restricted-syntax
         for (const error of errors) {
@@ -46,7 +25,7 @@ const ErrorHandlePlugin: ApolloServerPlugin = {
             && originalError
             && !isValidException(originalError)
           ) {
-            notify(originalError, requestContext);
+            notify(originalError, requestContext.context.req);
           }
         }
       },
