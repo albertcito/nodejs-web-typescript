77085ea5845a417c1fd2d06b9ceec04d6e2514e5
albertcito <alberttjornehoj@gmail.com>
2020-11-22 15:16:48 -0500

    procfile

--- src/init/server.ts
+++ src/init/server.ts
@@ -1,18 +1,18 @@
 import 'reflect-metadata';
 import express, { Express } from 'express';
-import { ConnectionOptions, createConnection } from 'typeorm';
 import dotenv from 'dotenv';
-import i18n from 'i18n';
-import { join } from 'path';
 import Bugsnag from '@bugsnag/js';
+import { join } from 'path';
+import i18n from 'i18n';
+import { ConnectionOptions, createConnection } from 'typeorm';
 
 import './i18n/index';
 import './bugsnag';
-import ormconfig from '../../ormconfig.json';
-import apolloServer from './graphql/public/server';
 import '../util/validatorjs/rules';
 import handleErrors from './handleErrors';
-import useControllersApi from './controllers';
+import ormconfig from '../../ormconfig.json';
+import apolloServer from './graphql/public/server';
+// import useControllersApi from './controllers';
 
 const getApp = async (): Promise<Express> => {
   dotenv.config();
@@ -32,7 +32,7 @@ const getApp = async (): Promise<Express> => {
   // Apollo graphQL
   await apolloServer(app, db);
   // Api
-  useControllersApi(app);
+  // useControllersApi(app);
   // handle global errors
   app.use(handleErrors);
   // This handles any errors that Express catches
