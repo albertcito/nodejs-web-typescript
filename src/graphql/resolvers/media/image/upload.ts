359b033308624fb2950429a7087a2d060f826dc8
albertcito <alberttjornehoj@gmail.com>
2021-01-15 17:36:43 -0500

    basic upload working

--- src/graphql/resolvers/media/image/upload.ts
+++ src/graphql/resolvers/media/image/upload.ts
@@ -3,11 +3,21 @@ import {
 } from 'type-graphql';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { v4 as uuidv4 } from 'uuid';
+import sharp from 'sharp';
 
 import Image from 'src/db/entities/Media/Image';
 import getExtension from 'src/util/files/getExtension';
 import getFileSize from 'src/util/files/getFileSize';
-import upload from 'src/util/files/upload';
+
+const getBuffer = (file: FileUpload) => {
+  const stream = file.createReadStream();
+  return new Promise<Buffer>((resolve, reject) => {
+    const buffers: Buffer[] = [];
+    stream.on('error', (error) => reject(error));
+    stream.on('data', (data: Buffer) => buffers.push(data));
+    stream.on('end', () => resolve(Buffer.concat(buffers)));
+  });
+};
 
 @Resolver()
 export default class UploadImageResolver {
@@ -21,12 +31,15 @@ export default class UploadImageResolver {
     image.path = 'uploads/images';
     image.mime = file.mimetype;
     image.slug = uuidv4();
-    image.width = 0;
-    image.height = 0;
     image.ext = await getExtension(file);
     image.size = await getFileSize(stream);
 
-    await upload(stream, image.fileName());
+    const imageBuffer = await getBuffer(file);
+    const imageSharp = sharp(imageBuffer);
+    const metadata = await imageSharp.metadata();
+    image.width = metadata.width;
+    image.height = metadata.height;
+    await imageSharp.toFile(`uploads/images/${image.fileName()}`);
     await image.save();
     return image;
   }
